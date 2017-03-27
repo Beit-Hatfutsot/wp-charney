687a9ae364c780c7654f6ea376b8a3feafe24adf
Nir Goldberg <nir@htmline.com>
2017-03-27 14:06:39 +0300

    Default content page

--- wp-content/themes/charney/views/page/page.php
+++ wp-content/themes/charney/views/page/page.php
@@ -10,3 +10,25 @@
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 ?>
+
+<div class="content-section row">
+
+	<div class="col-sm-12">
+
+		<?php
+			/**
+			 * Section title
+			 */
+			echo '<h2 class="section-title">' . get_the_title() . '</h2>';
+		?>
+
+		<?php
+			/**
+			 * Section content
+			 */
+			the_content();
+		?>
+
+	</div>
+
+</div>
