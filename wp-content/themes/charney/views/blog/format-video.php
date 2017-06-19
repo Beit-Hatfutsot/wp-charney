27554dfdc3678f3f9962bb5631f55449c14af33a
Nir Goldberg <nir@htmline.com>
2017-06-19 15:16:02 +0300

    Video post item presentation

--- wp-content/themes/charney/views/blog/format-video.php
+++ wp-content/themes/charney/views/blog/format-video.php
@@ -12,12 +12,28 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 /**
  * Variables
  */
-$url	= get_field( 'acf-post-attributes_video_url' );
-$cover	= get_field( 'acf-post-attributes_video_cover_image' );
+$url			= get_field( 'acf-post-attributes_video_url' );
+$cover			= get_field( 'acf-post-attributes_video_cover_image' );
+$default_cover	= get_field( 'acf-general-options_default_video_cover_image', 'option' );
 
-if ( ! $url )
+if ( ! $url || ! ( $cover || $default_cover ) )
 	return;
+
+$image = $cover ? $cover : $default_cover;
 
 ?>
 
-<iframe src="<?php echo $url; ?>"></iframe>
+<div class="blog-item blog-item-video">
+
+	<figure>
+		<img src="<?php echo $image['sizes']['medium']; ?>" width="<?php echo $image['sizes']['medium-width']; ?>" height="<?php echo $image['sizes']['medium-height']; ?>" alt="<?php the_title(); ?>" />
+		<figcaption>
+			<?php the_title(); ?>
+		</figcaption>
+
+		<div class="icon icon-play">
+			<?php get_template_part( 'views/svgs/shape', 'play' ); ?>
+		</div>
+	</figure>
+
+</div>
