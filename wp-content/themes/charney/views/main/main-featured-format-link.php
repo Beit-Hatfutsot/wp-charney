7381b531858a5bb513ad22cd35c94359a0b0e222
Nir Goldberg <nir@htmline.com>
2017-07-12 13:05:53 +0300

    Main page - modify link format featured item to display PDF preview whenever it possible

--- wp-content/themes/charney/views/main/main-featured-format-link.php
+++ wp-content/themes/charney/views/main/main-featured-format-link.php
@@ -13,32 +13,46 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
  * Variables
  */
 if ( function_exists('get_field') ) {
-	$url	= get_field( 'acf-post-attributes_google_url', $post_id );
+	$drive_item_id	= get_field( 'acf-post-attributes_google_drive_id',	$post_id );
+	$url			= get_field( 'acf-post-attributes_google_url',		$post_id );
 }
+
+// Build thumbnail URL
+$thumbnail = $drive_item_id && $url ? 'https://drive.google.com/thumbnail?authuser=0&sz=w450&id=' . $drive_item_id : '';
 
 ?>
 
 <div class="blog-item blog-item-link <?php echo $url ? 'blog-item-modal' : 'no-url'; ?>" <?php echo $url ? 'data-item-id="" data-type="link" data-toggle="modal" data-title="' . esc_html( get_the_title( $post_id ) ) . '" data-excerpt="' . esc_html( get_the_excerpt( $post_id ) ) . '" data-url="' . $url . '" data-target="#archive-items-modal"' : '' ; ?>>
 
-	<div class="icon icon-pdf">
-		<?php
-			/**
-			 * Display pdf icon
-			 */
-			get_template_part( 'views/svgs/shape', 'pdf' );
-		?>
-	</div>
+	<?php if ( $thumbnail ) { ?>
 
-	<div class="item-title">
-		<?php
-			if ( ! $url ) {
-				printf( __( '<strong>Error loading file:</strong> %s', 'charney' ), esc_html( get_the_title( $post_id ) ) );
-			} else {
-				echo esc_html( get_the_title( $post_id ) );
-			}
-		?>
-	</div>
+		<figure>
+			<img src="<?php echo $thumbnail; ?>" alt="<?php echo esc_html( get_the_title( $post_id ) ); ?>" />
+		</figure>
 
-	<div class="clearfix"></div>
+	<?php } else { ?>
+
+		<div class="icon icon-pdf">
+			<?php
+				/**
+				 * Display pdf icon
+				 */
+				get_template_part( 'views/svgs/shape', 'pdf' );
+			?>
+		</div>
+
+		<div class="item-title">
+			<?php
+				if ( ! $url ) {
+					printf( __( '<strong>Error loading file:</strong> %s', 'charney' ), esc_html( get_the_title( $post_id ) ) );
+				} else {
+					echo esc_html( get_the_title( $post_id ) );
+				}
+			?>
+		</div>
+
+		<div class="clearfix"></div>
+
+	<?php } ?>
 
 </div>
